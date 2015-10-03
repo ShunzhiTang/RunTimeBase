a281fb3b6b1c154df1e90ddc5191f61a3484604b
唐枫 <tsz@qq.com>
2015-10-03 10:58:34 -0700

    runTime实现方法的交换

--- RunTimeBase/RunTimeBase/main.m
+++ RunTimeBase/RunTimeBase/main.m
@@ -1,16 +1,18 @@
 //
 //  main.m
+//  RunTimeBase
+//
+//  Created by mac on 15-10-3.
+//  Copyright (c) 2015年 tsz. All rights reserved.
+//
 
 #import <Foundation/Foundation.h>
-//两个和运行时有关的头文件
-#import <objc/runtime.h>
-#import <objc/message.h>
+
 #import "Student.h"
 
+#import <objc/message.h>
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        //运行时的实现两个自定义方法的交换，就是说在我们执行的过程中，虽然执行的是run而结果确实study
-        
         Student *student = [[Student alloc]init];
         //打印
         [student study];
@@ -27,25 +29,6 @@ int main(int argc, const char * argv[]) {
         method_exchangeImplementations(m3, m1);
         
         [student study];//打印跑步
-        
-        
     }
     return 0;
 }
-//通过clang -rewrite-objc  main.m 可以知道 NSObject *object = [[NSObject alloc]init];转换为下列的c++语言
-/**
- int main(int argc, const char * argv[]) {
-{ __AtAutoreleasePool __autoreleasepool;
-     
-     NSObject *object = ((NSObject *(*)(id, SEL))(void *)objc_msgSend)((id)((NSObject *(*)(id, SEL))(void *)objc_msgSend)((id)objc_getClass("NSObject"), sel_registerName("alloc")), sel_registerName("init"));
-     
- }
-return 0;
-}
-替换：
- 
- NSObject *obj = objc_msgSend(objc_getClass(@"NSObject"),sel_registerName("alloc"));
- 
- objc_msgSend(obj ,sel_registerName("init"));
-
- */
